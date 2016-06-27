d363e9c0326f3349a77b76dc7d6323cab695d07b
Pinworm45 <Pinworm45@hotmail.com>
2016-06-27 01:10:51 -0400

    Publish

---
+++ Source/SoS/SoSGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SoSGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOS_API ASoSGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
